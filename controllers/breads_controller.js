68ab6fa8ba6a188ea54d4ed042a7342419d35928
Jason Garcia <jaygarcia0530@gmail.com>
2022-04-02 12:34:31 -0700

    Need to debug

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,67 +4,80 @@ const Bread = require('../models/bread.js')
 
 //INDEX
 breads.get('/', (req,res)=>{
-
-    res.render('index',{breads: Bread})
-    //res.send(Bread)
-
-})
-
-breads.get('/new', (req,res)=>{
-  res.render('new')
-})
-
-// EDIT
-breads.get('/:indexArray/edit', (req, res) => {
-  res.render('edit', {
-    bread: Bread[req.params.indexArray],
-    index: req.params.indexArray
-  })
-})
-
-
-// SHOW
-breads.get('/:arrayIndex', (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread:Bread[req.params.arrayIndex],
-      index: req.params.arrayIndex,
+  Bread.find()
+    .then(foundBreads =>{
+      console.log(foundBreads)
+      res.render('index',
+      { 
+        breads: foundBreads, 
+        title: 'Index Page'
+      })
     })
-  } else {
-    res.render('404')
-  }
 })
 
 // CREATE
 breads.post('/', (req, res) => {
+  console.log(req.body.name)
+  req.body.name = req.body.name[0]
   if (!req.body.image) {
-    req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+    req.body.image = undefined
   }
   if(req.body.hasGluten === 'on') {
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  Bread.push(req.body)
+  Bread.create(req.body)
   res.redirect('/breads')
 })
 
-//DELETE
-breads.delete('/:indexArray', (req,res)=>{
-  Bread.splice(req.params.indexArray,1)
-  res.status(303).redirect('/breads')
+//NEW
+breads.get('/new', (req,res)=>{
+  res.render('new')
+})
+
+// SHOW
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+   .then(foundBread=>{
+    console.log(foundBread)
+      res.render('Show', {
+        bread:foundBread
+    })
+  })
+})
+
+// EDIT
+breads.get('/:id/edit', (req, res) => {
+  Bread.findById(req.params.id)
+   .then(foundBreads=>{
+    res.render('edit', {
+      bread: foundBreads
+    })
+   })
+  
 })
 
 // UPDATE
-breads.put('/:arrayIndex', (req, res) => {
+breads.put('/:id', (req, res) => {
   if(req.body.hasGluten === 'on'){
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  Bread[req.params.arrayIndex] = req.body
-  res.redirect(`/breads/${req.params.arrayIndex}`)
+  Bread.findByIdAndUpdate(req.params.id,req.body,{new: true})
+   .then(updatedBread=>{
+     res.redirect(`/breads${req.params.id}`)
+   })
 })
 
+//DELETE
+breads.delete('/:id', (req,res)=>{
+  Bread.findByIdAndDelete(req.params.id)
+    .then(deletedBread=>{
+      console.log('Deleted Bread: ', deletedBread)
+      res.status(303).redirect('/breads')
+    })
+})
 
 module.exports = breads;
